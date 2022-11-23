9357f1335a31e980c91789a3da827e0968a3904c
Men Sheng <ydfyh@qq.com>
2022-11-23 11:57:08 +0800

    Add librime static lib file with Emscripten.

---
+++ build/src/rime/build_config.h
@@ -0,0 +1,14 @@
+//
+// Copyright RIME Developers
+// Distributed under the BSD License
+//
+#ifndef RIME_BUILD_CONFIG_H_
+#define RIME_BUILD_CONFIG_H_
+
+/* #undef RIME_BOOST_SIGNALS2 */
+#define RIME_ENABLE_LOGGING
+
+#define RIME_DATA_DIR "/usr/share/rime-data"
+#define RIME_PLUGINS_DIR "/usr/lib/rime-plugins"
+
+#endif  // RIME_BUILD_CONFIG_H_

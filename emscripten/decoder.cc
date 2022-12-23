14eaf712dad1358f299ff7ec983ed3fd312aa0e2
mensheng <feixiangmeng@outlook.com>
2022-12-23 17:03:16 +0800

    Add chrome os example support.

--- emscripten/decoder.cc
+++ emscripten/decoder.cc
@@ -20,7 +20,7 @@ void on_message(void* context_object,
       const char* message_value
 ) {
   EM_ASM({
-    rimeNotificationHandler(UTF8ToString($0), UTF8ToString($1));
+    imeHandler.onNotification(UTF8ToString($0), UTF8ToString($1));
   }, message_type, message_value);
 }
 
@@ -63,6 +63,75 @@ class Decoder {
         fprintf(stderr, "Error creating rime session.\n");
       }
     }
+
+    void notify_update() {
+      if (get_commit()) {
+        EM_ASM({
+          imeHandler.commitText(UTF8ToString($0));
+        }, commit_->text);
+
+        rime_->free_commit(commit_);
+      }
+
+      if (get_status() && status_->is_composing) {
+        if (get_context() && context_->composition.length > 0) {
+          const char* preedit = context_->composition.preedit;
+          if (!preedit) return;
+          size_t len = strlen(preedit);
+          size_t start = context_->composition.sel_start;
+          size_t end = context_->composition.sel_end;
+          size_t cursor = context_->composition.cursor_pos;
+
+          EM_ASM({
+            imeHandler.setComposition({
+              preedit: UTF8ToString($0),
+              start: $1,
+              end: $2,
+              cursor: $3
+            });
+          }, preedit, start, end, cursor);
+        }
+
+        if (context_->menu.num_candidates > 0) {
+          EM_ASM({
+            imeHandler.setCandidateWindowProperties({
+              selectKeys: UTF8ToString($0),
+              pageSize: $1,
+              pageNo: $2,
+              isLastPage: $3,
+              hightlighed: $4,
+              candidatesSize: $5
+            });
+          }, 
+            context_->menu.select_keys,
+            context_->menu.page_size, 
+            context_->menu.page_no,
+            context_->menu.is_last_page,
+            context_->menu.highlighted_candidate_index,
+            context_->menu.num_candidates
+          );
+
+          for (int i = 0; i < context_->menu.num_candidates; ++i) {
+            EM_ASM({
+              imeHandler.addCandidate(UTF8ToString($0), UTF8ToString($1));
+            }, 
+              context_->menu.candidates[i].text, 
+              context_->menu.candidates[i].comment
+            );
+          }
+
+          rime_->free_context(context_);
+        }
+
+        rime_->free_status(status_);
+      } else {
+        EM_ASM({
+          imeHandler.clearComposition();
+          imeHandler.hide();
+        }, 0);
+      }
+    }
+
 
     bool destroy_session() {
       return rime_->destroy_session(session_id_);
@@ -142,13 +211,50 @@ class Decoder {
       return rime_->get_status(session_id_, &status);
     }
 
-
 };
 
 
 EMSCRIPTEN_BINDINGS(rime_decoder) {
   class_<Decoder>("Decoder")
     .constructor<bool, bool>()
-    .function("decode", &Decoder::decode)
-    .function("processKey", &Decoder::process_key);
+    .function("processKey", &Decoder::process_key)
+    // .function("status", &Decoder::status)
+    .function("notifyUpdate", &Decoder::notify_update)
+    ;
+
+  // // RimeMenu
+  // value_object<RimeMenu>("RimeMenu")
+  //   .field("pageSize", &RimeMenu::page_size)
+  //   .field("pageNo", &RimeMenu::page_no)
+  //   .field("isLastPage", &RimeMenu::is_last_page)
+  //   .field("highlighted", &RimeMenu::highlighted_candidate_index)
+  //   .field("candidatesNum", &RimeMenu::num_candidates)
+  //   .field("candidates", &RimeMenu::candidates, allow_raw_pointers())
+  //   .field("selectKeys", &RimeMenu::select_keys)
+  //   ;
+  
+  // // RimeCandidate
+  // value_object<RimeCandidate>("RimeCandidate")
+  //   .field("text", &RimeCandidate::text)
+  //   .field("commit", &RimeCandidate::comment)
+  //   .field("reserved", &RimeCandidate::reserved)
+  //   ;
+
+  // // RimeContext
+  // value_object<RimeContext>("RimeContext")
+  //   .field("composition", &RimeContext::composition)
+  //   .field("menu", &RimeContext::menu)
+  //   .field("commitTextPreview", &RimeContext::commit_text_preview)
+  //   .field("selectLabels", &RimeContext::select_labels)
+  //   ;
+  
+  // // RimeComposition
+  // value_object<RimeComposition>("RimeComposition")
+  //   .field("length", &RimeComposition::length)
+  //   .field("cursorPos", &RimeComposition::cursor_pos)
+  //   .field("selectStart", &RimeComposition::sel_start)
+  //   .field("selectEnd", &RimeComposition::sel_end)
+  //   .field("preedit", &RimeComposition::preedit)
+  //   ;
+  
 }
--- emscripten/pre.js
+++ emscripten/pre.js
@@ -1,49 +1,279 @@
 
-function initFS() {
-  let depsId = "fs initFS";
-  FS.mkdir("/build");
-  FS.mount(IDBFS, {root: "."}, "/build");
-  Module.addRunDependency(depsId);
-  FS.syncfs(true, (err) => {
-    if (err) reject(err);
-    try {
-      FS.stat("/build/installation.yaml");
-      FS.symlink("/build/installation.yaml", "/installation.yaml");
-      FS.symlink("/build/user.yaml", "/user.yaml");
-      FS.symlink("/build/luna_pinyin.userdb", "/luna_pinyin.userdb");
-      // FS.symlink("/build/cangjie5.userdb", "/cangjie5.userdb");
-    } catch(e) {
-      console.log("No install.");
+// function initFS() {
+//   let depsId = "fs initFS";
+//   FS.mkdir("/build");
+//   FS.mount(IDBFS, {root: "."}, "/build");
+//   Module.addRunDependency(depsId);
+//   FS.syncfs(true, (err) => {
+//     if (err) reject(err);
+//     try {
+//       FS.stat("/build/installation.yaml");
+//       FS.symlink("/build/installation.yaml", "/installation.yaml");
+//       FS.symlink("/build/user.yaml", "/user.yaml");
+//       FS.symlink("/build/luna_pinyin.userdb", "/luna_pinyin.userdb");
+//       // FS.symlink("/build/cangjie5.userdb", "/cangjie5.userdb");
+//     } catch(e) {
+//       console.log("No install.");
+//     }
+//     Module.removeRunDependency(depsId);
+
+//   });
+  
+// }
+
+
+Module['onRuntimeInitialized'] = function() {
+  console.log("onRuntimeInitialized");
+  imeHandler.decoder = new Module['Decoder'](false, false);
+
+}
+
+
+const keyNormalMapping = {
+  Esc: "Escape",
+  Backspace: "BackSpace",
+  " ": "space",
+  Ctrl: "Control",
+  ControlLeft: "Control_L",
+  ControlRight: "Control_R",
+  ShiftLeft: "Shift_L",
+  ShiftRight: "Shift_R",
+  AltLeft: "Alt_L",
+  AltRight: "Alt_R",
+  "`": "grave",
+  Enter: "Return"
+}
+
+
+class IMEHandler {
+  port = undefined;
+  decoder = undefined;
+  
+  contextID = -1;
+  engineID = "";
+  candidates = [];
+  candidatesSize = 0;
+
+  visible = false;
+  shiftLock = false;
+  _lastKeyIsShift = false;
+
+
+  onActivate(engineID) {
+    this.engineID = engineID;
+  }
+
+  onFocus(context) {
+    this.contextID = context.contextID;
+    this.hide();
+    this.clearComposition();
+  }
+
+  checkDecoder() {
+    if (!this.decoder) {
+      this.decoder = new Module.decoder();
     }
-    Module.removeRunDependency(depsId);
-
-  });
-  
-}
-
-globalThis.rimeNotificationHandler =  function(type, value) {
-  if (Module['rimeNotificationHandler']) {
-    Module['rimeNotificationHandler'](type, value);
-  } else {
-    console.error("No notification handler.", type, value);
   }
-}
 
-globalThis.refreshFS = () => {
-  
-  try {
-    FS.stat("/build/installation.yaml");
-  } catch(e) {
-    console.error("Not found installation.yaml!");
-    ["installation.yaml", "user.yaml", "luna_pinyin.userdb", /**"cangjie5.userdb"*/].forEach((item) => {
-      FS.symlink(`/${item}`, `/build/${item}`)
+  handleShiftKey(keyEvent) {
+    if (keyEvent.shiftKey) {
+      if (keyEvent.key === "Shift") {
+        this._lastKeyIsShift = true;
+        return false;
+      } else if (this._lastKeyIsShift) {
+        this._lastKeyIsShift = false;
+        return false;
+      } else if (/^[A-Z]$/.test(keyEvent)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getKeyString(keyEvent) {
+    let key = "";
+    if (keyEvent.ctrlKey && keyEvent.key != "Ctrl") {
+      key += "Control+";
+    }
+    if (keyEvent.altKey && keyEvent.key != "Alt") {
+      key += "Alt+";
+    }
+    if (keyEvent.shiftKey && keyEvent.key != "Shift") {
+      key += "Shift+";
+    }
+    
+    key += Reflect.has(keyNormalMapping, keyEvent.key) ? keyNormalMapping[keyEvent.key] : keyEvent.key;
+    
+    return key;
+  }
+
+  onKeyEvent(engineID, keyEvent, requestId) {
+    this.engineID = engineID;
+    this.checkDecoder();
+
+    if (keyEvent.type === "keydown") {
+      if (this.handleShiftKey(keyEvent)) {
+        return this.handleResponse(requestId, false);
+      }
+
+      if (this.shiftLock || keyEvent.capsLock) {
+        return this.handleResponse(requestId, false);
+      }
+
+      if (keyEvent.ctrlKey && keyEvent.key == "Ctrl"
+      || keyEvent.shiftKey && keyEvent.key == "Shift"
+      || keyEvent.altKey && keyEvent.key == "Alt") {
+        return this.handleResponse(requestId, false);
+      }
+      
+      let key = this.getKeyString(keyEvent);
+      
+      let response = this.decoder.processKey(key);
+      this.handleResponse(requestId, !!response);
+
+    } else {
+      if (keyEvent.key === "Shift" && this._lastKeyIsShift) {
+        this.shiftLock = !this.shiftLock;
+      }
+
+      this.handleResponse(requestId, false);
+    }
+  }
+
+  onDeactivate() {
+    this.decoder.delete();
+    this.decoder = undefined;
+    this.hide();
+  }
+
+  hide() {
+    this.visible = false;
+    this.handleMethod("setCandidateWindowProperties", [{
+      engineID: this.engineID,
+      properties: {
+        visible: false
+      }
+    }]);
+    this.handleMethod("hideInputView", []);
+  }
+
+  clearComposition() {
+    this.handleMethod("clearComposition", [{contextID: this.contextID}]);
+  }
+
+  commitText(text) {
+    this.visible = false;
+    this.handleMethod("commitText", [{
+      contextID: this.contextID,
+      text
+    }]);
+  }
+
+  setComposition(obj) {
+    this.handleMethod("setComposition", [
+      {
+        contextID: this.contextID,
+        cursor: obj['cursor'],
+        selectionStart: obj['start'],
+        selectionEnd: obj['end'],
+        text: obj['preedit']
+      }
+    ])
+  }
+
+  setCandidateWindowProperties(obj) {
+    this.candidates = [];
+    this.candidatesSize = obj['candidatesSize'];
+
+    if (this.visible) return;
+    if (this.candidatesSize == 0) return this.hide();
+    this.handleMethod("setCandidateWindowProperties", [
+      {
+        engineID: this.engineID,
+        properties: {
+          visible: true,
+          cursorVisible: true,
+          vertical: true,
+          pageSize: obj['pageSize']
+        }
+      }
+    ])
+    this.visible = true;
+  }
+
+  addCandidate(candidate, annotation) {
+    
+    let id = this.candidates.length + 1;
+    this.candidates.push({
+      candidate,
+      id,
+      annotation,
+      label: "" + id
     });
+
+    if (this.candidates.length === this.candidatesSize) {
+      this.handleMethod("setCandidates", [{
+        contextID: this.contextID,
+        candidates: this.candidates
+      }]);
+    }
   }
-  FS.syncfs(false, () => {});
+
+  handleMethod(type, value) {
+    if (!this.port) return;
+    this.port.postMessage({
+      data: {
+        type,
+        value
+      }
+    })
+  }
+
+  handleResponse(requestId, response) {
+    this.handleMethod("keyEventHandled", [
+      requestId,
+      response
+    ]);
+
+    if (response) {
+      this.decoder.notifyUpdate();
+    }
+  }
+
+  onNotification(type, value) {
+    console.log(type, value);
+  }
+
 }
 
-window.onclose = function() {
-  refreshFS();
-}
+globalThis.imeHandler = new IMEHandler();
 
-Module.preRun.push(initFS);
+chrome.runtime.onConnectExternal.addListener((port) => {
+  imeHandler.port = port;
+  
+  port.onMessage.addListener((msg, port) => {
+    let {type, value} = msg.data;
+    if (type in imeHandler) {
+      imeHandler[type](...value);
+    }
+  })
+})
+
+// globalThis.refreshFS = () => {
+  
+//   try {
+//     FS.stat("/build/installation.yaml");
+//   } catch(e) {
+//     console.error("Not found installation.yaml!");
+//     ["installation.yaml", "user.yaml", "luna_pinyin.userdb", /**"cangjie5.userdb"*/].forEach((item) => {
+//       FS.symlink(`/${item}`, `/build/${item}`)
+//     });
+//   }
+//   FS.syncfs(false, () => {});
+// }
+
+// window.onclose = function() {
+//   refreshFS();
+// }
+
+// Module.preRun.push(initFS);

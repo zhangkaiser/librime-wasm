40b063d9dc046d14ca1837d977d2735693552844
mensheng <feixiangmeng@outlook.com>
2023-02-04 15:28:24 +0800

    Fix build error.

--- emscripten/cmake-adapter.sh
+++ emscripten/cmake-adapter.sh
@@ -1,7 +1,4 @@
 
-
-export EM_CMAKE_PATH=/home/mensheng/app/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
-
 sed -i 's/set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)/#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)/' \
   ${EM_CMAKE_PATH}
 sed -i 's/set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)/#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)/' \
